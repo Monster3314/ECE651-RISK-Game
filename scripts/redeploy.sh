2295194b8fff0b05fe94b3e3a130cebae52af46e
Zhiwei Zhou <drewryzhou@gmail.com>
2022-03-10 00:41:01 -0500

    update the scripts

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -5,5 +5,5 @@ docker kill riskGame
 docker rm riskGame
 
 # now run the new one
-docker run -d --name riskGame -p 1651:1651 -t ece651 ./gradlew run
+docker run -d --name riskGame -p 1651:1651 -t ece651 ./gradlew :server:run --args="2"
 
