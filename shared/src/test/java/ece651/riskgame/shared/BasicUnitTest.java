2083b1bedb922777cf7c71abf5c5801c8c265c60
Zhiwei Zhou <drewryzhou@gmail.com>
2022-03-12 23:50:42 -0500

    update unit

--- shared/src/test/java/ece651/riskgame/shared/BasicUnitTest.java
+++ shared/src/test/java/ece651/riskgame/shared/BasicUnitTest.java
@@ -6,10 +6,20 @@ import org.junit.jupiter.api.Test;
 
 public class BasicUnitTest {
   @Test
-  public void test_getAttack_getName() {
-    BasicUnit bu = new BasicUnit();
+  public void test_getAttack_getName_getNum() {
+    BasicUnit bu = new BasicUnit(10);
     assertEquals(1, bu.getAttack());
     assertEquals(1, bu.getHP());
+    assertEquals(10, bu.getNum());
   }
 
+  @Test
+  public void test_add_dec() {
+    BasicUnit bu = new BasicUnit(20);
+    bu.addSoldiers(10);
+    assertEquals(30, bu.getNum());
+    bu.decSoldiers(15);
+    assertEquals(15, bu.getNum());
+    assertThrows(IllegalArgumentException.class, () -> bu.decSoldiers(16));
+  }
 }
