e58f33d0b9c1b8fe1813450a8d2489de62e88072
zz258@duke.edu <zz258@vcm-25372.vm.duke.edu>
2022-03-13 22:56:57 -0400

    add test cases for RiskGame

--- server/src/main/java/ece651/riskgame/server/RiskGame.java
+++ server/src/main/java/ece651/riskgame/server/RiskGame.java
@@ -83,7 +83,7 @@ public class RiskGame {
       List<Unit> needToAssign = new ArrayList<>();
       needToAssign.add(new BasicUnit(unitNumber));
       ObjectOutputStream oos = oosMap.get(player.getKey());
-      oos.writeObject(needToAssign);
+      oos.writeObject(needToAssign); // send List of Unit to be allocated to client
 
       ObjectInputStream ois = oisMap.get(player.getKey());
       Map<Territory, List<Unit>> assignResult = (Map<Territory, List<Unit>>) ois.readObject();
@@ -91,7 +91,10 @@ public class RiskGame {
       for (Territory t : clan.getOccupies()) {
         if (assignResult.containsKey(t)) {
           t.addUnitList(assignResult.get(t));
-        } else throw new IllegalArgumentException("player " + player.getValue() + " did not assign units to " + t.getName() +"!");
+        }
+        else {
+          throw new IllegalArgumentException("player " + player.getValue() + " did not assign units to " + t.getName() +"!");
+        }
       }
     }
   }
