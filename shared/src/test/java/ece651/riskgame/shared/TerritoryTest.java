3b326c0fc97ba0096d8dcdd7001b66bb30addb7f
Zhiwei Zhou <drewryzhou@gmail.com>
2022-03-17 23:18:20 -0400

    fix territory test code

--- shared/src/test/java/ece651/riskgame/shared/TerritoryTest.java
+++ shared/src/test/java/ece651/riskgame/shared/TerritoryTest.java
@@ -24,16 +24,16 @@ public class TerritoryTest {
     assertEquals(1, t.getUnits().get(0).getNum());
 
     t.decUnit(new BasicUnit(1));
-    assertEquals(0, t.getUnits().size());
+    assertEquals(0, t.getUnits().get(0).getNum());
     
     t.addUnit(new DemoUnit(1));
-    assertEquals(1, t.getUnits().get(0).getNum());
+    assertEquals(1, t.getUnits().get(1).getNum());
 
     t.addUnit(new DemoUnit(1));
-    assertEquals(2, t.getUnits().get(0).getNum());
+    assertEquals(2, t.getUnits().get(1).getNum());
 
     t.addUnit(new BasicUnit(1));
-    assertEquals(1, t.getUnits().get(1).getNum());
+    assertEquals(1, t.getUnits().get(0).getNum());
   }
 
 }
